768d368fe4dace5c870803cd13555c7568e6a6a4
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-09-07 13:28:40 +0800

    doc: Create new branch to test doc style

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -25,6 +25,7 @@ extensions = [  #'sphinx.ext.autodoc',
     "sphinx.ext.autosummary",
     "ext.autoexample",
     "aafigure.sphinxext",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -33,7 +34,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 # Font
